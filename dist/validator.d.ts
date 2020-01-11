f5b44803cc7562bf050aefd6aa9e82befd8ada32
dikac <rigel234@yahoo.com>
2020-01-11 12:24:11 +0700

    update deployment script

--- dist/validator.d.ts
+++ dist/validator.d.ts
@@ -1,4 +1,4 @@
-import Validatable from 't-validatable/validatable';
+import Validatable from '@dikac/t-validatable/validatable';
 export default interface Validator<Value, Result extends Validatable = Validatable> {
     validate(value: Value): Result;
 }
